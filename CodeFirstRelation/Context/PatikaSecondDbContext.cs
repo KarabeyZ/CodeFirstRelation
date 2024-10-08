637e8006c16c1345159407cf7aea5b6f1048fd00
Zekeriya KARABEY <karabeyzekeriya@gmail.com>
2024-10-08 13:35:08 +0300

    Add project files.

---
+++ CodeFirstRelation/Context/PatikaSecondDbContext.cs
@@ -0,0 +1,37 @@
+﻿using CodeFirstRelation.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstRelation.Data
+{
+    public class PatikaSecondDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ZEKERIYA\\SQLEXPRESS;Database=Library;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+       
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(user =>
+            {
+                user.ToTable("users");
+                user.HasKey(user => user.Id);
+                user.HasMany(u => u.Posts).WithOne(x => x.User).HasForeignKey(x => x.UserId);
+
+            });
+            modelBuilder.Entity<Post>(post =>
+            {
+                post.ToTable("post");
+                post.HasKey(post => post.Id);
+            });
+                         
+        }
+    }
+}
---
+++ CodeFirstRelation/Migrations/20241006132456_First.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirstRelation.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_post_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_post_UserId",
+                table: "post",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "post");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
